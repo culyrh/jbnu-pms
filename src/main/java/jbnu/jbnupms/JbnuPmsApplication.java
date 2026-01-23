3b79fec15faebd0b28ddd93c134d77154f235a3c
dohyeon <ehgus8910@gmail.com>
2026-01-23 21:12:38 +0900

    first commit

---
+++ src/main/java/jbnu/jbnupms/JbnuPmsApplication.java
@@ -0,0 +1,13 @@
+package jbnu.jbnupms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbnuPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbnuPmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jbnu/jbnupms/JbnuPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package jbnu.jbnupms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbnuPmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
